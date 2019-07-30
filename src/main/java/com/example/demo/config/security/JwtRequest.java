a246aacb651eae598fdb91f7f90c88045b55a1f9
Sharipov Ilia <ilya.sharipov@raiffeisen.ru>
2019-07-30 22:31:24 +0300

    85% code coverage

--- src/main/java/com/example/demo/config/security/JwtRequest.java
+++ src/main/java/com/example/demo/config/security/JwtRequest.java
@@ -4,14 +4,6 @@ public class JwtRequest {
     private String username;
     private String password;
 
-    public JwtRequest() {
-    }
-
-    public JwtRequest(String username, String password) {
-        this.setUsername(username);
-        this.setPassword(password);
-    }
-
     public String getUsername() {
         return this.username;
     }
